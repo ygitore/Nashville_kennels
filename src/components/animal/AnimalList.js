614389dcfb72a018e5d7a0239221a9bd64408465
yitbarekgitore <yitbareky@gmail.com>
2020-04-21 15:51:58 -0500

    refactered animal component to render animal information that has customer name and animal location in it

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -2,14 +2,29 @@ import React, { useContext } from "react"
 import { AnimalContext } from "./AnimalProvider"
 import Animal from "./Animal"
 import "./Animal.css"
+import { CustomerContext } from "../customer/CustomerProvider"
+import { LocationContext } from "../location/LocationProvider"
 
 export default () => {
     const { animals } = useContext(AnimalContext)
+    const { customers } = useContext(CustomerContext)
+    const { locations } = useContext(LocationContext)
 
     return (
         <div className="animals">
         {
-            animals.map(anima => <Animal key={anima.id} animal ={anima} />)
+            animals.map(
+                anima => {
+                    const owner = customers.find(customer => customer.id === anima.customerId)
+                    const clinic = locations.find(location => location.id === anima.locationId)
+
+                    return <Animal key={anima.id} 
+                        animal ={anima} 
+                        customer = {owner} 
+                        location = {clinic} />
+                }
+            )
+
         }
         </div>
     )
