bf0ebbd64467f5d651b1cebd8972fbe7f9f1540c
yitbarekgitore <yitbareky@gmail.com>
2020-04-23 10:47:11 -0500

    added form to add new customer and refactored customerList.js file

--- src/components/customer/CustomerList.js
+++ src/components/customer/CustomerList.js
@@ -1,16 +1,31 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import { CustomerContext } from "./CustomerProvider"
 import Customer from "./Customer"
 import "./Customer.css"
+import { Modal, ModalBody, ModalHeader } from "reactstrap"
+import CustomerForm from "./CustomerForm"
 
 export default () => {
     const { customers } = useContext(CustomerContext)
-
+    const [modal, setModal] = useState(false)
+    const toggle = () => setModal(!modal)
     return (
-        <div className="customers">
-        {
-            customers.map(cust => <Customer key={cust.id} customer ={cust} />)
-        }
-        </div>
+        <>
+            <h2>Customers</h2>
+            <div className = "fake_customer_link" onClick = {toggle}>Add New Customer</div>
+            <div className="customers">
+            {
+                customers.map(cust => <Customer key={cust.id} customer ={cust} />)
+            }
+            </div>
+            <Modal isOpen = {modal} toggle = {setModal}>
+                <ModalHeader toggle = {toggle}>
+                    <div>New Customer</div>
+                </ModalHeader>
+                <ModalBody>
+                    <CustomerForm  toggler = {toggle} />
+                </ModalBody>
+            </Modal>
+        </>
     )
 }
